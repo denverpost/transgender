2f34e903bffb26790fe0a50cecfd2bf9238e322c
Joe Murphy <joe.murphy@gmail.com>
2016-01-18 16:06:08 -0700

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -162,7 +162,7 @@ function playerCreator(embedId, playerId, divId, doDarkBack) {
         scrollDownTo('#overviewvid');
         vidBack = false;
     }
-    $('#' + embedId).html('<iframe src="http://launch.newsinc.com/?type=VideoPlayer/Single&widgetId=1&trackingGroup=69016&siteSection=denverpost_spl_fea_sty&videoId=' + playerId + '" class="informvideo" noscroll style="width:100%; height:100%;" frameborder="no" scrolling="no" noresize></iframe>');
+    $('#' + embedId).html('<iframe src="http://launch.newsinc.com/?type=VideoPlayer/Single&widgetId=1&trackingGroup=90115&siteSection=denverpost_spl_fea_sty&videoId=' + playerId + '" class="informvideo" noscroll style="width:100%; height:100%;" frameborder="no" scrolling="no" noresize></iframe>');
     /*
     OO.Player.create(embedId, playerId, {
         'autoplay':true,
